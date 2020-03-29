e4e2ae2be6b8cc58213923336bb55174e0f3b8f7
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-03-28 23:11:58 -0400

    Bug fixes in search

--- graphtage/search.py
+++ graphtage/search.py
@@ -1,8 +1,7 @@
-from abc import abstractmethod, ABC
-from typing import Callable, Generic, Iterator, Optional, TypeVar, Union
+from typing import Callable, Dict, Generic, Iterator, Optional, TypeVar, Union
 from typing_extensions import Protocol
 
-from .fibonacci import FibonacciHeap
+from .fibonacci import FibonacciHeap, HeapNode
 
 
 class Infinity:
@@ -77,17 +76,20 @@ class Range:
         if not self or not other:
             slb, sub = self.lower_bound is not None, self.upper_bound is not None
             olb, oub = other.lower_bound is not None, other.upper_bound is not None
-            if slb and olb:
-                return self.lower_bound < other.lower_bound
-            elif sub and olb:
+            if sub and olb:
                 return self.upper_bound < other.lower_bound
+            elif sub and oub:
+                return self.upper_bound < other.upper_bound
             else:
                 return False
-        return self.lower_bound < other.lower_bound or \
-            (self.lower_bound == other.lower_bound and self.upper_bound < other.upper_bound)
+        return self.upper_bound < other.upper_bound or \
+            (self.upper_bound == other.upper_bound and self.lower_bound < other.lower_bound)
 
     def __le__(self, other):
         return self < other or self == other
+
+    def dominates(self, other) -> bool:
+        return self.upper_bound <= other.lower_bound
 
     def hash(self):
         return hash((self.lower_bound, self.upper_bound))
@@ -133,6 +135,16 @@ class Range:
         return f"[{self.lower_bound}, {self.upper_bound}]"
 
 
+class MaxRange(Range):
+    def __init__(self, range: Range):
+        super().__init__(range.lower_bound, range.upper_bound)
+        self._range = range
+
+    def __lt__(self, other):
+        assert isinstance(other, MaxRange)
+        return not (self._range <= other._range)
+
+
 class Bounded(Protocol):
     tighten_bounds: Callable[[], bool]
     bounds: Callable[[], Range]
@@ -145,17 +157,38 @@ class IterativeTighteningSearch(Generic[B]):
     def __init__(self,
                  possibilities: Iterator[B],
                  initial_bounds: Optional[Range] = None):
-        def get_range(bounded: B) -> Range:
+        def get_range(bounded: Bounded) -> Range:
             return bounded.bounds()
+
+        def negated_range(bounded: Bounded) -> Range:
+            bounds: Bounded = bounded.bounds()
+            #return Range(-1 * bounds.upper_bound, -1 * bounds.lower_bound)
+            return MaxRange(bounds)
 
         self._unprocessed: Iterator[B] = possibilities
         self._untightened: FibonacciHeap[B, Range] = FibonacciHeap(key=get_range)
         self._tightened: FibonacciHeap[B, Range] = FibonacciHeap(key=get_range)
-        self.best_match: B = None
+        self._best: FibonacciHeap[B, Range] = FibonacciHeap(key=get_range)
+        self._worst: FibonacciHeap[B, Range] = FibonacciHeap(key=negated_range)
+        self._worst_nodes: Dict[HeapNode[B, Range], HeapNode[B, Range]] = {}
+        #self.best_match: B = None
+        #self.worst_match: B = None
         if initial_bounds is None:
             self.initial_bounds = Range(NegativeInfinity, PositiveInfinity)
         else:
             self.initial_bounds = initial_bounds
+
+    @property
+    def best_match(self) -> B:
+        if not self._best or self._unprocessed is not None:
+            return None
+        return self._best.peek()
+
+    @property
+    def worst_match(self) -> B:
+        if not self._worst or self._unprocessed is not None:
+            return None
+        return self._worst.peek()
 
     def search(self) -> B:
         while self.tighten_bounds():
@@ -166,7 +199,45 @@ class IterativeTighteningSearch(Generic[B]):
         if self.best_match is None:
             return self.initial_bounds
         else:
-            return self.best_match.bounds()
+            if self._unprocessed is None and self._best:
+                lb = min(node.item.bounds().lower_bound for node in self._best.nodes())
+                assert lb >= self.initial_bounds.lower_bound
+            else:
+                lb = self.initial_bounds.lower_bound
+            return Range(min(lb, self.best_match.bounds().upper_bound), self.best_match.bounds().upper_bound)
+
+    def _delete_node(self, node: HeapNode[B, Range]):
+        self._best.decrease_key(node, Range(NegativeInfinity, NegativeInfinity))
+        node.deleted = True
+        self._best.pop()
+        worst_node = self._worst_nodes[node]
+        self._worst.decrease_key(worst_node, MaxRange(Range(PositiveInfinity, PositiveInfinity)))
+        worst_node.deleted = True
+        self._worst.pop()
+        del self._worst_nodes[node]
+
+    def _update_bounds(self, node: HeapNode[B, Range]):
+        if self.best_match is not None \
+                and self.best_match != node.item \
+                and self.best_match.bounds().dominates(node.item.bounds()):
+            self._delete_node(node)
+            return
+        bounds = node.item.bounds()
+        if bounds.lower_bound > node.key.lower_bound:
+            # The lower bound increased, so we need to remove and re-add the node
+            # because the Fibonacci heap only permits making keys smaller
+            self._best.decrease_key(node, Range(NegativeInfinity, NegativeInfinity))
+            self._best.pop()
+            best = self._best.push(node.item)
+            self._worst_nodes[best] = self._worst_nodes[node]
+            del self._worst_nodes[node]
+            node = best
+        if bounds.upper_bound < node.key.upper_bound:
+            worst = self._worst_nodes[node]
+            self._worst.decrease_key(worst, MaxRange(Range(PositiveInfinity, PositiveInfinity)))
+            self._worst.pop()
+            worst = self._worst.push(node.item)
+            self._worst_nodes[node] = worst
 
     def tighten_bounds(self) -> bool:
         starting_bounds = self.bounds()
@@ -175,44 +246,41 @@ class IterativeTighteningSearch(Generic[B]):
                 try:
                     next_best: B = next(self._unprocessed)
                     if self.initial_bounds.lower_bound > NegativeInfinity and \
-                            self.initial_bounds.lower_bound > next_best.bounds().upper_bound:
+                            self.initial_bounds.lower_bound >= next_best.bounds().upper_bound:
                         # We can't do any better than this choice!
                         self.best_match = next_best
                         self._untightened.clear()
                         self._tightened.clear()
                         self._unprocessed = None
                         return True
-                    if starting_bounds.upper_bound <= next_best.bounds().lower_bound:
+                    if starting_bounds.dominates(next_best.bounds()) or \
+                            (self.best_match is not None
+                             and self.best_match.bounds().dominates(next_best.bounds())):
                         # No need to add this new edit if it is strictly worse than the current best!
                         pass
                     else:
-                        self._untightened.push(next_best)
+                        best = self._best.push(next_best)
+                        worst = self._worst.push(next_best)
+                        self._worst_nodes[best] = worst
                 except StopIteration:
                     self._unprocessed = None
-            if self._untightened:
-                while self._untightened.peek().tighten_bounds():
-                    pass
-                self._tightened.push(self._untightened.pop())
-            if self._tightened:
-                assert self.best_match is None or self.best_match.bounds() >= self._tightened.peek().bounds()
-                self.best_match = self._tightened.peek()
-                if self._unprocessed is None:
-                    if not self._untightened:
-                        self._tightened.clear()
-                        return True
-                    elif len(self._tightened) >= 2:
-                        # does the min node dominate the second smallest? if so, we are done!
-                        self._tightened.pop()
-                        second_smallest = self._tightened.peek()
-                        if self.best_match.bounds().upper_bound < second_smallest.bounds().lower_bound <= PositiveInfinity:
-                            self._unprocessed = None
-                            self._untightened.clear()
-                            self._tightened.clear()
-                            return True
-                        else:
-                            self._tightened.push(self.best_match)
-
-            if starting_bounds != self.bounds():
-                return True
-            elif self._unprocessed is None and not self._untightened and not self._tightened:
+            tightened = False
+            for node in list(self._best.nodes()):
+                if node.deleted:
+                    continue
+                bounds_before: Range = node.key
+                if not node.item.tighten_bounds():
+                    if self.best_match is not None \
+                            and self.best_match != node.item \
+                            and self.best_match.bounds().dominates(bounds_before):
+                        self._delete_node(node)
+                    continue
+                assert node.item.bounds().lower_bound >= bounds_before.lower_bound
+                assert node.item.bounds().upper_bound <= bounds_before.upper_bound
+                self._update_bounds(node)
+                tightened = True
+            if self._unprocessed is None and not tightened:
                 return False
+            elif starting_bounds.lower_bound < self.bounds().lower_bound \
+                    or starting_bounds.upper_bound > self.bounds().upper_bound:
+                return True
--- test/test_search.py
+++ test/test_search.py
@@ -1,6 +1,8 @@
 import random
 from unittest import TestCase
 
+from tqdm import tqdm, trange
+
 from graphtage.search import Bounded, Range, IterativeTighteningSearch
 
 
@@ -42,15 +44,42 @@ class TestIterativeTighteningSearch(TestCase):
             while r.tighten_bounds():
                 next_range = r.bounds()
                 self.assertTrue(next_range.lower_bound >= last_range.lower_bound
-                                or next_range.upper_bound <= last_range.upper_bound)
+                                and next_range.upper_bound <= last_range.upper_bound
+                                and (
+                                    next_range.lower_bound > last_range.lower_bound or
+                                    next_range.upper_bound < last_range.upper_bound
+                                ))
                 last_range = next_range
 
     def test_iterative_tightening_search(self):
-        for _ in range(1000):
-            ranges = [RandomDecreasingRange() for _ in range(100)]
-            best_range: RandomDecreasingRange = None
-            for r in ranges:
-                if best_range is None or r.final_value < best_range.final_value:
-                    best_range = r
-            search = IterativeTighteningSearch(iter(ranges))
-            self.assertEqual(best_range.final_value, search.search().final_value)
+        speedups = 0
+        tests = 0
+        try:
+            t = trange(1000)
+            for test_num in t:
+                ranges = [RandomDecreasingRange() for _ in range(100)]
+                best_range: RandomDecreasingRange = None
+                for r in ranges:
+                    if best_range is None or r.final_value < best_range.final_value:
+                        best_range = r
+                search = IterativeTighteningSearch(iter(ranges))
+                substatus = tqdm(desc=str(search.bounds()))
+                while search.tighten_bounds():
+                    substatus.total = search.bounds().upper_bound - search.bounds().lower_bound
+                    substatus.desc = f"Test {test_num}: {search.bounds()!s}"
+                    substatus.update(substatus.total - (search.bounds().upper_bound - search.bounds().lower_bound))
+                substatus.clear()
+                result = search.best_match
+                tightenings = sum(r.tightenings for r in ranges)
+                untightened = 0
+                for r in ranges:
+                    t_before = r.tightenings
+                    while r.tighten_bounds():
+                        pass
+                    untightened += r.tightenings - t_before
+                t.desc = f"{(untightened + tightenings) / tightenings:.01f}x Speedup"
+                speedups += (untightened + tightenings) / tightenings
+                tests += 1
+                self.assertEqual(best_range.final_value, result.final_value)
+        finally:
+            print(f"Average speedup: {speedups / tests:.01f}x")
