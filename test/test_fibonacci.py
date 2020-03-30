8033d5f1926ae03bbd91474778fe0be50e78acf6
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-03-30 12:23:18 -0400

    More FibHeap unit tests

--- test/test_fibonacci.py
+++ test/test_fibonacci.py
@@ -1,10 +1,19 @@
 import random
+from collections import defaultdict
+from typing import Dict, List
 from unittest import TestCase
 
-from graphtage.fibonacci import FibonacciHeap
+from tqdm import tqdm, trange
+
+from graphtage.fibonacci import FibonacciHeap, HeapNode
 
 
 class TestFibonacciHeap(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.random_list: List[int] = [random.randint(0, 10000) for _ in range(10000)]
+        cls.sorted_list: List[int] = sorted(cls.random_list)
+
     def test_duplicate_items(self):
         heap = FibonacciHeap()
         test_list = [2, 1, 2]
@@ -13,11 +22,58 @@ class TestFibonacciHeap(TestCase):
         heap_sorted = [heap.pop() for _ in range(len(test_list))]
         self.assertEqual(sorted(test_list), heap_sorted)
 
-    def test_fibonacci_heap(self):
-        heap = FibonacciHeap()
-        random_list = [random.randint(0, 10000) for _ in range(10000)]
-        sorted_list = sorted(random_list)
-        for rand_int in random_list:
+    def random_heap(self) -> FibonacciHeap[int, int]:
+        heap: FibonacciHeap[int, int] = FibonacciHeap()
+        for rand_int in self.random_list:
             heap.push(rand_int)
-        heap_sorted = [heap.pop() for _ in range(len(random_list))]
-        self.assertEqual(sorted_list, heap_sorted)
+        return heap
+
+    def test_fibonacci_heap(self):
+        heap = self.random_heap()
+        heap_sorted = [heap.pop() for _ in range(len(self.random_list))]
+        self.assertEqual(self.sorted_list, heap_sorted)
+
+    def test_node_traversal(self):
+        heap = self.random_heap()
+        self.assertEqual(sum(1 for _ in heap.nodes()), len(heap))
+
+    def test_node_deletion(self):
+        heap = self.random_heap()
+        for i in trange(len(self.random_list)//4):
+            random_node: HeapNode[int, int] = random.choice(list(heap.nodes()))
+            heap.decrease_key(random_node, -1)
+            heap.pop()
+            random_node.deleted = True
+            self.assertEqual(len(heap), len(self.random_list) - i - 1)
+
+    def test_decrease_key(self):
+        heap = self.random_heap()
+        nodes_by_value: Dict[int, set(HeapNode[int, int])] = defaultdict(set)
+        for node in heap.nodes():
+            nodes_by_value[node.key].add(node)
+        changes: Dict[int, int] = {}
+        for _ in trange(len(self.random_list)//4):
+            while True:
+                random_sorted_index = random.randint(0, len(self.random_list) - 1)
+                if random_sorted_index not in changes:
+                    break
+            random_node: HeapNode[int, int] = next(iter(nodes_by_value[self.sorted_list[random_sorted_index]]))
+            self.assertEqual(random_node.key, self.sorted_list[random_sorted_index])
+            if random_node.key <= 0:
+                continue
+            new_key = random.randint(0, random_node.key - 1)
+            nodes_by_value[random_node.key].remove(random_node)
+            nodes_by_value[new_key].add(random_node)
+            changes[random_sorted_index] = new_key
+            heap.decrease_key(random_node, new_key)
+        updated_list = []
+        for i, expected in enumerate(self.sorted_list):
+            if i in changes:
+                updated_list.append(changes[i])
+            else:
+                updated_list.append(expected)
+        expected_list = sorted(updated_list)
+        for expected in tqdm(expected_list):
+            node = heap.min_node
+            heap.pop()
+            self.assertEqual(node.key, expected)
