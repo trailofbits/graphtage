2158f1d59bbea17716765e176f0d2d759995bf29
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-03-31 14:55:51 -0400

    Cleaned up the search test status

--- test/test_search.py
+++ test/test_search.py
@@ -63,11 +63,14 @@ class TestIterativeTighteningSearch(TestCase):
                     if best_range is None or r.final_value < best_range.final_value:
                         best_range = r
                 search = IterativeTighteningSearch(iter(ranges))
-                with tqdm(desc=str(search.bounds())) as substatus:
-                    while search.tighten_bounds():
-                        substatus.total = search.bounds().upper_bound - search.bounds().lower_bound
-                        substatus.desc = f"Test {test_num}: {search.bounds()!s}"
-                        substatus.update(substatus.total - (search.bounds().upper_bound - search.bounds().lower_bound))
+                # with tqdm(desc=str(search.bounds()), leave=False) as substatus:
+                #     while search.tighten_bounds():
+                #         substatus.total = search.bounds().upper_bound - search.bounds().lower_bound
+                #         substatus.desc = f"Test {test_num}: {search.bounds()!s}"
+                #         substatus.update(substatus.total - (search.bounds().upper_bound - search.bounds().lower_bound))
+                #     substatus.clear()
+                while search.tighten_bounds():
+                    pass
                 result = search.best_match
                 tightenings = sum(r.tightenings for r in ranges)
                 untightened = 0
