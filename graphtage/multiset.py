e75abb1a7d78714090b990b89a0d3273c35a8655
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-05-12 09:53:31 -0400

    Docstrings for the MultiSet module

--- graphtage/multiset.py
+++ graphtage/multiset.py
@@ -1,3 +1,10 @@
+"""A module for representing an edit on a multiset.
+
+This is used by :class:`graphtage.MultiSetNode` and :class:`graphtage.DictNode`, since the latter is a multiset containg
+:class:`graphtage.KeyValuePairNode` objects.
+
+"""
+
 from typing import Iterator, List
 
 from .bounds import Range
@@ -9,6 +16,12 @@ from .utils import HashableCounter
 
 
 class MultiSetEdit(SequenceEdit):
+    """An edit matching one unordered collection of items to another.
+
+    It works by using a :class:`graphtage.matching.WeightedBipartiteMatcher` to find the minimum cost matching from
+    the elements of one collection to the elements of the other.
+
+    """
     def __init__(
             self,
             from_node: SequenceNode,
@@ -16,8 +29,21 @@ class MultiSetEdit(SequenceEdit):
             from_set: HashableCounter[TreeNode],
             to_set: HashableCounter[TreeNode]
     ):
+        """Initializes the edit.
+
+        Args:
+            from_node: Any sequence node from which to match.
+            to_node: Any sequence node to which to match.
+            from_set: The set of nodes from which to match. These should typically be children of :obj:`from_node`, but
+                this is neither checked nor enforced.
+            to_set: The set of nodes to which to match. These should typically be children of :obj:`to_node`, but this
+                is neither checked nor enforced.
+
+        """
         self.to_insert = to_set - from_set
+        """The set of nodes in :obj:`to_set` that do not exist in :obj:`from_set`."""
         self.to_remove = from_set - to_set
+        """The set of nodes in :obj:`from_set` that do not exist in :obj:`to_set`."""
         to_match = from_set & to_set
         self._edits: List[Edit] = [Match(n, n, 0) for n in to_match.elements()]
         self._matcher = WeightedBipartiteMatcher(
@@ -32,6 +58,7 @@ class MultiSetEdit(SequenceEdit):
         )
 
     def is_complete(self) -> bool:
+        """A :class:`MultiSetEdit` is complete after the first call to :meth:`MultiSetEdit.tighten_bounds`."""
         # The edits are ready after the first call to self.tighten_bounds()
         return self._is_tightened
 
@@ -49,6 +76,12 @@ class MultiSetEdit(SequenceEdit):
             yield Insert(to_insert=ins, insert_into=self.from_node)
 
     def tighten_bounds(self) -> bool:
+        """Delegates to :meth:`WeightedBipartiteMatcher.tighten_bounds`.
+
+         The matching is complete after the first call to this function, so it also sets
+         :meth:`self.is_complete<MultiSetEdit.is_complete>` to :const:`True`.
+
+         """
         self._is_tightened = True
         return self._matcher.tighten_bounds()
 
