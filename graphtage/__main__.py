33bcc9c967b8c9558b3b49c84f8df101769cf88b
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-06-09 11:11:37 -0400

    Adds support for the '.json5' extension (#17)

--- graphtage/__main__.py
+++ graphtage/__main__.py
@@ -239,6 +239,8 @@ def main(argv=None) -> int:
         mimetypes.suffix_map['.yml'] = '.yaml'
     elif '.yaml' not in mimetypes.types_map:
         mimetypes.suffix_map['.yaml'] = '.yml'
+    if '.json5' not in mimetypes.types_map:
+        mimetypes.add_type('application/json5', '.json5')
 
     if args.from_mime is not None:
         from_mime = args.from_mime
