c7e1f162d066435118737c92e764ea287f2b270c
GitHub Action <action@github.com>
2020-12-06 15:33:33 +0000

    Update documentation for refs/heads/master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.2.4',
+    VERSION: '0.2.5-git',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
